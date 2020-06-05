f1c9c00d379d7c5b45a3e4104c33680a8ae5865c
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2020-06-05 11:07:21 +0100

    Updates

---
+++ precache-manifest.014a3ed191843a90bb90ce4b616eab03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5073345f4a2c05f92ee64fac85d17d7f",
+    "url": "/hngi7-task2-frontend/index.html"
+  },
+  {
+    "revision": "a2a8ed6ebd755b7d6798",
+    "url": "/hngi7-task2-frontend/static/css/main.f5f99838.chunk.css"
+  },
+  {
+    "revision": "485d16ab110231617c6c",
+    "url": "/hngi7-task2-frontend/static/js/2.cb8d9530.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hngi7-task2-frontend/static/js/2.cb8d9530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a8ed6ebd755b7d6798",
+    "url": "/hngi7-task2-frontend/static/js/main.34aec2e0.chunk.js"
+  },
+  {
+    "revision": "71f57cb5b74ed512618e",
+    "url": "/hngi7-task2-frontend/static/js/runtime-main.c59763d8.js"
+  }
+]);
